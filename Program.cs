306123c6a7e1905adc2b9be12952bc5064cbd038
Ponikarovskiy <ponikarovsky.a@gmail.com>
2023-02-24 16:01:39 +0300

    Решение задачи № 50

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
+/*
 Console.Clear();
 System.Console.WriteLine();
 System.Console.WriteLine("*Задача № 47. Двумерный массив, заполненный случайными вещественными числами*");
@@ -46,6 +47,7 @@ int ReadInt(string message)
     System.Console.WriteLine(message);
     return Convert.ToInt32(Console.ReadLine());
 }
+*/
 
 
 // Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
@@ -54,6 +56,52 @@ int ReadInt(string message)
 // 5 9 2 3
 // 8 4 2 4
 // 1,7 -> такого числа в массиве нет (формат ввода произвольный, не обязательно через запятую)
+
+
+Console.Clear();
+System.Console.WriteLine();
+System.Console.WriteLine("*Задача № 50. Найти число в массиве*");
+System.Console.WriteLine();
+int rows = ReadInt("Введите индекс строки: ");
+int colums = ReadInt("Введите индекс столбца: ");
+System.Console.WriteLine();
+int[,] numbers = new int[5, 10];
+FillArray2D(numbers);
+PrintArray2D(numbers);
+
+if (rows < numbers.GetLength(0) && colums < numbers.GetLength(1)) Console.WriteLine(numbers[rows, colums]);
+else Console.WriteLine($"{rows}, {colums} -> такого числа в массиве нет");
+
+void FillArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+System.Console.WriteLine();
 
 // Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 // Например, задан массив:
