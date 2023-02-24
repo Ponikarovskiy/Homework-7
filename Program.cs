62be09ea1084ad5a69c8cc849a526fc110f0aac9
Ponikarovskiy <ponikarovsky.a@gmail.com>
2023-02-24 15:41:20 +0300

    Решение задачи № 47

--- Program.cs
+++ Program.cs
@@ -4,6 +4,50 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
+Console.Clear();
+System.Console.WriteLine();
+System.Console.WriteLine("*Задача № 47. Двумерный массив, заполненный случайными вещественными числами*");
+System.Console.WriteLine();
+
+int rows = ReadInt("Введите количество строк: ");
+int colums = ReadInt("Введите количество столбцов: ");
+double[,] numbers = new double[rows, colums];
+FillArray2D(numbers);
+PrintArray2D(numbers);
+
+
+void FillArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-99, 99) / 10.0;
+        }
+    }
+}
+
+
+void PrintArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.WriteLine(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
 // Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
 // Например, задан массив:
 // 1 4 7 2
